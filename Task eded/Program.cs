01254328dd89f65bdfd9720d936247fc30d847a1
qjale <tu7knjupn@code.edu.az>
2022-03-05 11:31:19 +0400

    feat:task

---
+++ Task eded/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Task_eded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //Console.WriteLine ("sozu daxil edin: ");
+            //string soz = Console.ReadLine().ToLower();
+            //Console.WriteLine ("lazim olan herfi daxil edin: ");
+            //char herf = char.Parse(Console.ReadLine().ToLower());
+            //int count = 0;
+            //for (int i = 0; i < soz.Length; i++)
+
+            //{
+
+            //    if (soz[i] == herf)
+
+            //    {
+
+            //        count++;
+
+            //    }
+
+            //}
+
+            //Console.WriteLine (string.Format("'{0}' herfinden '{1}' dene var.", herf, count));
+            #endregion Task1
+            #region Task2
+
+
+
+
+            //int sum = 0;
+            //int[] numbers= { 13,27,33,35,39};
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+                   
+            //       sum += i;
+              
+            //    }
+            //    Console.WriteLine("3-e bolunenleri cemle" + sum);
+            //}
+            
+
+
+            #endregion Task2
+            #region Task3
+
+            //int a = 555455;
+            //int count = 0;
+            //while (a>=1)
+            //{
+
+            //    a=a / 10;
+            //    count++;
+
+                
+            //}
+            
+            
+            //Console.WriteLine(count);
+
+
+
+            #endregion Task3
+        }
+    }
+}
